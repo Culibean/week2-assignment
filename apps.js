27f5665c186ff6600d21885599751ceecb112ddd
ULRIKE KUNZE <ULRIKE.E.KUNZE@GMAIL.COM>
2025-11-28 09:54:47 +0000

    chore: added JS script functions to create images, thumbnails and fullscreen versions.

---
+++ apps.js
@@ -0,0 +1,51 @@
+console.log("Hello World");
+
+//TODO: Add in images to JS with JS arrays
+
+const imageData = [
+  {
+    imageName: "boysOnStreet",
+    imageSrc: "./images/boys_on_street.webp",
+    imageAlt: "Boys playing on wintery street",
+  },
+  {
+    imageName: "childWithCat",
+    imageSrc: "./images/child_with _cat.webp",
+    imageAlt: "young girl in Christmas outfit next to a white cat",
+  },
+  {
+    imageName: "childrenOnSledge",
+    imageSrc: "./images/DE_Christmas.webp",
+    imageAlt: "two children on a sledge with German writing underneath",
+  },
+];
+
+//TODO: Create thumbnails of above uimages through a function, including a loop
+
+function createThumbnails() {
+  const thumbnail = document.getElementById("thumbnail-container");
+  imageData.forEach(function (picture) {
+    const img = document.createElement("img");
+    img.src = picture.imageSrc;
+    img.alt = picture.imageAlt;
+    img.className = "thumbnail";
+    img.addEventListener("click", function () {
+      createFullscreenImages(picture);
+    });
+    thumbnail.appendChild(img);
+  });
+}
+
+//TODO: create fullscreen Function and add it as an event listener to the thumbnail function
+
+function createFullscreenImages(picture) {
+  const fullscreen = document.getElementById("fullscreen-container");
+  fullscreen.innerHTML = "";
+  const fullImg = document.createElement("img");
+  fullImg.src = picture.imageSrc;
+  fullImg.alt = picture.imageAlt;
+  fullImg.className = "fullscreen";
+  fullscreen.appendChild(fullImg);
+}
+
+createThumbnails();
